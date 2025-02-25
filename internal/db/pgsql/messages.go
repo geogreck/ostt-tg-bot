01dee43a4269060bd7b2a41a8b8eec08ca91cf67
Georgy <georgre4ko@ya.ru>
2025-02-25 12:58:06 +0300

    fix

--- internal/db/pgsql/messages.go
+++ internal/db/pgsql/messages.go
@@ -111,7 +111,8 @@ func (md *MessagesDatabase) GetTopMessages(chatId int64) ([]models.Message, erro
 		SELECT message_id, reaction_banana_count, reaction_like_count, reaction_like_count+2*reaction_banana_count, author_nickname, sent_at AS score
 		FROM tg_messages
 		WHERE chat_id = $1
-		ORDER BY score DESC;
+		ORDER BY score DESC
+		LIMIT 20;
 	`
 	rows, err := md.db.Query(query, chatId)
 	if err != nil {
